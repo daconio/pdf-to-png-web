b474cb979eda6330cc8e45b4611fd80cab72616b
kookjinkim <user@example.com>
2026-01-18 08:55:55 +0900

    fix(vercel): configure next.config.ts for dynamic vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 const repo = process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '';
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: isGithubActions ? 'export' : undefined,
   basePath: isGithubActions ? repo : '',
   images: {
     unoptimized: true,
